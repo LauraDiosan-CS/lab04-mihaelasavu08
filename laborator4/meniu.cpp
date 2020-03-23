b5cbfe46b1c8e5ae181d65ffef2b582feaed0e1f
mihaelasavu08 <mihaelasavu08@gmail.com>
2020-03-23 08:51:29 -0700

    no message

---
+++ laborator4/meniu.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "meniu.h"
+using namespace std;
+void show_menu() {
+	cout << "0. iesire" << endl;
+	cout << "1. adaugare produs" << endl;
+	cout << "2. afisare produse " << endl;
+	cout << "3. modificare produs" << endl;
+	cout << "4. stergere produs" << endl;
+
+
+}
